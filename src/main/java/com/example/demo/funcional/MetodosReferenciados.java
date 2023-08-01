b5a33568f332d540969b8563aa2a80d29f4fd017
AnddyQG <anddysk819@gmail.com>
2023-07-31 23:17:40 -0500

    tarea 15

---
+++ src/main/java/com/example/demo/funcional/MetodosReferenciados.java
@@ -0,0 +1,49 @@
+package com.example.demo.funcional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MetodosReferenciados {
+
+	private static final Logger LOG = LoggerFactory.getLogger(MetodosReferenciados.class); // buena practica usar este
+																							// logger para
+
+	// Supplier
+	public Integer getId() {
+
+		return 8;
+	}
+
+	// Consumer
+	public void aceptar(String arg) {
+		String cadena = "Nombre";
+
+		LOG.info(cadena + " " + arg);
+	}
+	// predicate
+
+	public boolean evaluar(Integer arg) {
+		if (arg.compareTo(2) == 0) {
+			return true;
+		} else {
+			return false;
+		}
+
+	};
+	
+	//function
+	
+	public String aplicar(Integer arg) {
+		
+		return "Complemento " + arg;
+	};
+	
+	
+	//unary
+	public Integer agregar(Integer arg) {
+		
+		return arg+arg*2;
+		
+	};
+
+}
